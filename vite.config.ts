a9b466697cbf9ea768b18dcba233b855da00fa18
umairatl <umairatlwork@gmail.com>
2024-05-07 15:42:11 +0800

    clean code

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
-        
